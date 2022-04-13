f45188f5d5b16971a4043c69eb2f35cb9286b7a9
nakagawa1210 <yusuke2020@s.okayama-u.ac.jp>
2022-04-13 19:30:09 +0900

    Add count mutex

--- services/c-tcp-broker/server.c
+++ services/c-tcp-broker/server.c
@@ -27,7 +27,12 @@ volatile int data_num = 0;
 volatile int recv_num = 0;
 
 int msg_len[MAX_COUNT] = {0};
+
 pthread_mutex_t mutex;
+int recv_lock_cnt = 0;
+int send_lock_cnt = 0;
+uint64_t recv_lock_time[MAX_COUNT][2] = {0};
+uint64_t send_lock_time[MAX_COUNT][2] = {0};
 
 static void die(const char *msg)
 {
@@ -76,9 +81,16 @@ int store_msg(struct message *msg)
   uint64_t log_tsc;
   log_tsc = getclock();
   msg_assign_time_stamp(msg, log_tsc, SERVER_RECV);
-  pthread_mutex_lock(&mutex);
+
+  if(pthread_mutex_trylock(&mutex) != 0){
+    send_lock_time[send_lock_cnt][0] = getclock();
+    pthread_mutex_lock(&mutex);
+    send_lock_time[send_lock_cnt][1] = getclock();
+    send_lock_cnt += 1;
+  }
   msg_ary[data_num] = *msg;
   pthread_mutex_unlock(&mutex);
+
   data_num++;
 
   return 0;
@@ -94,8 +106,12 @@ int shift_msg(struct message *msg, uint32_t ws)
   }
   msg_len[recv_num] = data_num - recv_num;
 
-
-  pthread_mutex_lock(&mutex);
+  if(pthread_mutex_trylock(&mutex) != 0){
+    recv_lock_time[recv_lock_cnt][0] = getclock();
+    pthread_mutex_lock(&mutex);
+    recv_lock_time[recv_lock_cnt][1] = getclock();
+    recv_lock_cnt += 1;
+  }
   *msg = msg_ary[recv_num];
   pthread_mutex_unlock(&mutex);
 
@@ -145,10 +161,17 @@ void *loop(void *pArg)
     }
     break;
     case RECV_ACK:
-      // for (int i = 0; i < recv_num; i++)
-      // {
-      //   printf("%d,%d\n", i, msg_len[i]);
-      // }
+      {
+	uint64_t send_lock_sum = 0, recv_lock_sum = 0;
+	printf("send_lock,%d,recv_lock,%d\n", send_lock_cnt, recv_lock_cnt);
+	for (int i = 0; i < recv_lock_cnt; i++){
+	  recv_lock_sum += recv_lock_time[i][1] - recv_lock_time[i][0];
+	}
+	for (int i = 0; i < send_lock_cnt; i++){
+	  send_lock_sum += send_lock_time[i][1] - send_lock_time[i][0];
+	}
+	printf("send_lock_time,%d,recv_lock_time,%d\n", send_lock_sum, recv_lock_sum);
+      }
       break;
     case HELLO_REQ:
       net_send_ack(fd, &msg.payload, HELLO_ACK, msg.hdr.fragments, msg.hdr.saddr, msg.hdr.daddr);
