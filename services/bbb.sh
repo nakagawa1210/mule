51f6f9f470017f7447c9c9c9476fc4ac6935530f
nakagawa1210 <yusuke2020@s.okayama-u.ac.jp>
2022-05-17 19:03:43 +0900

    Add sn,rn option to bbb.sh

--- services/bbb.sh
+++ services/bbb.sh
@@ -8,6 +8,8 @@ SEND_WINDOWSIZE=1
 RECV_LANGUAGE=C
 RECV_WINDOWSIZE=1
 BROKER_LANGUAGE=C
+SEND_NUMBER=1
+RECV_NUMBER=1
 
 function check_arg {
     if [ $# != 0 ]; then
@@ -24,9 +26,12 @@ PORT_NUMBER             $PORT_NUMBER
 COMMUNICATION_METHOD    $COMMUNICATION_METHOD
 SEND_LANGUAGE           $SEND_LANGUAGE
 SEND_WINDOWSIZE         $SEND_WINDOWSIZE
+SEND_NUMBER             $SEND_NUMBER
 RECV_LANGUAGE           $RECV_LANGUAGE
 RECV_WINDOWSIZE         $RECV_WINDOWSIZE
+RECV_NUMBER             $RECV_NUMBER
 BROKER_LANGUAGE         $BROKER_LANGUAGE
+
 EOF
 }
 
@@ -48,11 +53,13 @@ Options:
     -rl RECV_LANGUAGE          receiver language C or Ruby
     -rw RECV_WINDOWSIZE        receiver windowsize
     -bl BROKER_LANGUAGE        broker language C or Ruby
+    -sn SEND_NUMBER            number of sender
+    -rn RECV_NUMBER            number of receiver
 EOF
 }
 
 #参考サイト https://qiita.com/b4b4r07/items/dcd6be0bb9c9185475bb
-OPT=`getopt -o hc:m: -l bh:,bp:,sl:,sw:,rl:,rw:,bl: -a -- "$@"`
+OPT=`getopt -o hc:m: -l bh:,bp:,sl:,sw:,rl:,rw:,bl:,sn:,rn: -a -- "$@"`
 if [ $? != 0 ] ; then
     exit 1
 fi
@@ -91,6 +98,12 @@ do
 	--bl) BROKER_LANGUAGE=$2
             shift 2
             ;;
+	--sn) SEND_NUMBER=$2
+            shift 2
+            ;;
+	--rn) RECV_NUMBER=$2
+            shift 2
+            ;;
 	--) shift
             break
             ;;
@@ -106,6 +119,10 @@ dump_params
 
 TIME=$(date "+%Y%m%d_%H%M")
 echo start "$MSGS"_"$COMMUNICATION_METHOD"_$SEND_WINDOWSIZE-$SEND_LANGUAGE-$RECV_WINDOWSIZE-$RECV_LANGUAGE-"$BROKER_LANGUAGE"_$TIME.log $(date "+%H:%M:%S")
+
+SEND_MSGS=$(expr $MSGS / $SEND_NUMBER)
+RECV_MSGS=$(expr $MSGS / $RECV_NUMBER)
+RECVID=()
 
 if [ $COMMUNICATION_METHOD = "TCP" ]; then
     if [ $HOST_NAME != "localhost" ]; then
@@ -127,24 +144,41 @@ if [ $COMMUNICATION_METHOD = "TCP" ]; then
             echo "Broker Language argument invalid value"
 	fi
     fi
+    
     sleep 1
+    
     if [ $RECV_LANGUAGE = "C" ]; then
-        ./c-tcp-broker/recv_client $MSGS $RECV_WINDOWSIZE $HOST_NAME $PORT_NUMBER > log/"$MSGS"_"$COMMUNICATION_METHOD"_$SEND_WINDOWSIZE-$SEND_LANGUAGE-$RECV_WINDOWSIZE-$RECV_LANGUAGE-"$BROKER_LANGUAGE"_$TIME.log &
-        RECVID=$!
+	for i in $(seq 1 $RECV_NUMBER)
+	do
+	    ./c-tcp-broker/recv_client $RECV_MSGS $RECV_WINDOWSIZE $HOST_NAME $PORT_NUMBER > log/"$MSGS"_"$COMMUNICATION_METHOD"_$SEND_WINDOWSIZE-$SEND_LANGUAGE-$RECV_WINDOWSIZE-$RECV_LANGUAGE-"$BROKER_LANGUAGE"_$TIME_"$i".log &
+            RECVID+=($!)
+	done
     elif [ $RECV_LANGUAGE = "Ruby" ]; then
-        ruby ruby-tcp-broker/recv_client.rb $MSGS $RECV_WINDOWSIZE $HOST_NAME $PORT_NUMBER > log/"$MSGS"_"$COMMUNICATION_METHOD"_$SEND_WINDOWSIZE-$SEND_LANGUAGE-$RECV_WINDOWSIZE-$RECV_LANGUAGE-"$BROKER_LANGUAGE"_$TIME.log &
-        RECVID=$!
+	for i in $(seq 1 $RECV_NUMBER)
+	do
+	    ruby ruby-tcp-broker/recv_client.rb $RECV_MSGS $RECV_WINDOWSIZE $HOST_NAME $PORT_NUMBER > log/"$MSGS"_"$COMMUNICATION_METHOD"_$SEND_WINDOWSIZE-$SEND_LANGUAGE-$RECV_WINDOWSIZE-$RECV_LANGUAGE-"$BROKER_LANGUAGE"_$TIME_"$i".log &
+            RECVID+=($!)
+	done    
     else
         echo "Receiver Language argument invalid value"
     fi
+
     sleep 1
+
     if [ $SEND_LANGUAGE = "C" ]; then
-        ./c-tcp-broker/send_client $MSGS $SEND_WINDOWSIZE $HOST_NAME $PORT_NUMBER
+	for i in $(seq 1 $SEND_NUMBER)
+	do
+	    ./c-tcp-broker/send_client $SEND_MSGS $SEND_WINDOWSIZE $HOST_NAME $PORT_NUMBER
+	done		 
     elif [ $SEND_LANGUAGE = "Ruby" ]; then
-        ruby ruby-tcp-broker/send_client.rb $MSGS $SEND_WINDOWSIZE $HOST_NAME $PORT_NUMBER
+	for i in $(seq 1 $SEND_NUMBER)
+	do
+            ruby ruby-tcp-broker/send_client.rb $SEND_MSGS $SEND_WINDOWSIZE $HOST_NAME $PORT_NUMBER
+	done
     else
         echo "Sender Language argument invalid value"
     fi
+    
 elif [ $COMMUNICATION_METHOD = "gRPC" ]; then
     if [ $HOST_NAME != "localhost" ]; then
 	if [ $BROKER_LANGUAGE = "C" ]; then
@@ -165,21 +199,37 @@ elif [ $COMMUNICATION_METHOD = "gRPC" ]; then
             echo "Broker Language argument invalid value"
 	fi
     fi
+    
     sleep 1
+    
     if [ $RECV_LANGUAGE = "C" ]; then
-        ./c-grpc-broker/recv_client $MSGS $RECV_WINDOWSIZE $HOST_NAME $PORT_NUMBER > log/"$MSGS"_"$COMMUNICATION_METHOD"_$SEND_WINDOWSIZE-$SEND_LANGUAGE-$RECV_WINDOWSIZE-$RECV_LANGUAGE-"$BROKER_LANGUAGE"_$TIME.log &
-        RECVID=$!
+	for i in $(seq 1 $RECV_NUMBER)
+	do
+            ./c-grpc-broker/recv_client $MSGS $RECV_WINDOWSIZE $HOST_NAME $PORT_NUMBER > log/"$MSGS"_"$COMMUNICATION_METHOD"_$SEND_WINDOWSIZE-$SEND_LANGUAGE-$RECV_WINDOWSIZE-$RECV_LANGUAGE-"$BROKER_LANGUAGE"_$TIME_"$i".log &
+            RECVID+=($!)
+	done
     elif [ $RECV_LANGUAGE = "Ruby" ]; then
-        ruby ruby-grpc-broker/recv_client.rb $MSGS $RECV_WINDOWSIZE $HOST_NAME $PORT_NUMBER > log/"$MSGS"_"$COMMUNICATION_METHOD"-$SEND_WINDOWSIZE-$SEND_LANGUAGE-$RECV_WINDOWSIZE-$RECV_LANGUAGE-"$BROKER_LANGUAGE"_$TIME.log &
-        RECVID=$!
+	for i in $(seq 1 $RECV_NUMBER)
+	do
+            ruby ruby-grpc-broker/recv_client.rb $MSGS $RECV_WINDOWSIZE $HOST_NAME $PORT_NUMBER > log/"$MSGS"_"$COMMUNICATION_METHOD"-$SEND_WINDOWSIZE-$SEND_LANGUAGE-$RECV_WINDOWSIZE-$RECV_LANGUAGE-"$BROKER_LANGUAGE"_$TIME_"$i".log &
+            RECVID+=($!)
+	done
     else
         echo "Receiver Language argument invalid value"
     fi
+    
     sleep1
+    
     if [ $SEND_LANGUAGE = "C" ]; then
-        ./c-grpc-broker/send_client $MSGS $SEND_WINDOWSIZE $HOST_NAME $PORT_NUMBER
+	for i in $(seq 1 $SEND_NUMBER)
+	do
+            ./c-grpc-broker/send_client $SEND_MSGS $SEND_WINDOWSIZE $HOST_NAME $PORT_NUMBER
+	done
     elif [ $SEND_LANGUAGE = "Ruby" ]; then
-        ruby ruby-grpc-broker/send_client.rb $MSGS $SEND_WINDOWSIZE $HOST_NAME $PORT_NUMBER
+	for i in $(seq 1 $SEND_NUMBER)
+	do
+            ruby ruby-grpc-broker/send_client.rb $SEND_MSGS $SEND_WINDOWSIZE $HOST_NAME $PORT_NUMBER
+	done
     else
         echo "Sender Language argument invalid value"
     fi
@@ -187,13 +237,15 @@ else
     echo "Communication Method argument invalid value"
 fi
 
-while(true)
-do
-    if [ $(ps -p $RECVID | wc -l) = "1" ]; then
-    break
-    fi
-    sleep 1
-done
+wait ${RECVID[@]}
+
+#while(true)
+#do
+#    if [ $(ps -p $RECVID | wc -l) = "1" ]; then
+#    break
+#    fi
+#    sleep 1
+#done
 
 if [ $HOST_NAME != "localhost" ]; then
     ssh nakagawa@hsc1.swlab.cs.okayama-u.ac.jp killall server
